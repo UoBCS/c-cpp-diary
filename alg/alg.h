7ea2e3b30cc6f87249a220ac87b06ec6d64c3d8d
oss6 <ossedb@gmail.com>
2016-01-09 23:13:49 +0000

    Added binary search

---
+++ alg/alg.h
@@ -0,0 +1,14 @@
+/*
+ * alg - Collection of algorithms
+ *
+ * Ossama Edbali <ossedb@gmail.com>
+ * MIT License 2016
+ *
+ */
+
+#ifndef ALG_H
+#define ALG_H
+
+int binary_search(const int *arr, int n, int val);
+
+#endif
