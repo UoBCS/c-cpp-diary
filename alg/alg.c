16e0978773fbd445131a8ca940dd044078d85e58
oss6 <ossedb@gmail.com>
2016-02-20 20:36:53 +0000

    Formatted code

--- alg/alg.c
+++ alg/alg.c
@@ -8,19 +8,17 @@
 
 #include "alg.h"
 
-int binary_search(const int *arr, int n, int val)
-{
+int binary_search(const int *arr, int n, int val) {
     int low = 0, high = n - 1, mid;
 
-    while (low <= high)
-    {
-	mid = (low + high) / 2;
-	if (arr[mid] > val)
-	    high = mid - 1;
-	else if (arr[mid] < val)
-	    low = mid + 1;
-	else
-	    return mid;
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (arr[mid] > val)
+            high = mid - 1;
+        else if (arr[mid] < val)
+            low = mid + 1;
+        else
+            return mid;
     }
 
     return -1;
