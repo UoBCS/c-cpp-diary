335285c1f1941c1cf29b70f199420d1906c9e699
oss6 <ossedb@gmail.com>
2016-02-21 17:03:16 +0000

    Reorganised project

--- src/strlib/strlib.c
+++ src/strlib/strlib.c
@@ -7,36 +7,32 @@
  */
 
 #include "strlib.h"
-#include "../util.h"
+#include "util.h"
 
-int str_len(char s[])
-{
+int str_len(char s[]) {
     int i = 0;
     while (s[i] != '\0')
         i++;
     return i;
 }
 
-int str_cmp(char *s, char *t)
-{
+int str_cmp(char *s, char *t) {
     int l1 = str_len(s),
-	l2 = str_len(t),
-	len = l1 > l2 ? l1 : l2,
-	i;
+            l2 = str_len(t),
+            len = l1 > l2 ? l1 : l2,
+            i;
 
-    for (i = 0; i < len; i++)
-    {
-	if (s[i] > t[i])
-	    return 1;
-	else if (s[i] < t[i])
-	    return -1;
+    for (i = 0; i < len; i++) {
+        if (s[i] > t[i])
+            return 1;
+        else if (s[i] < t[i])
+            return -1;
     }
-    
+
     return l1 > l2 ? 1 : ((l1 < l2) ? -1 : 0);
 }
 
-void squeeze(char s[], char c)
-{
+void squeeze(char s[], char c) {
     int i, j;
 
     for (i = j = 0; s[i] != '\0'; i++)
@@ -56,8 +52,7 @@ int contains(char s[], char c) {
     return 0;
 }
 
-void str_cat(char s[], char t[])
-{
+void str_cat(char s[], char t[]) {
     int i = 0, j = 0;
 
     while (s[i] != '\0')
@@ -66,20 +61,18 @@ void str_cat(char s[], char t[])
     while ((s[i++] = t[j++]) != '\0');
 }
 
-void reverse(char s[])
-{
+void reverse(char s[]) {
     int c, i, j;
 
     for (i = 0, j = str_len(s) - 1; i < j; i++, j--)
-	swap(&s[i], &s[j]);
+        swap(&s[i], &s[j]);
 }
 
-int atoi(char *s)
-{
+int atoi(char *s) {
     int res = 0, i;
 
     for (i = 0; *(s + i) != '\0'; i++)
-	res = res * 10 + *(s + i) - '\0';
+        res = res * 10 + *(s + i) - '\0';
 
-    return res
+    return res;
 }
